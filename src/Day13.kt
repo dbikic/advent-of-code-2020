95b936ed0d39641a0645f6c3e584f00b9b0b2aad
Dino Bikic <dino.bikic@websummit.com>
2022-12-06 19:50:22 +0100

    Day 13

---
+++ src/Day13.kt
@@ -0,0 +1,43 @@
+import java.awt.Desktop
+import java.io.File
+import java.net.URL
+import java.net.URLEncoder
+
+fun main() {
+    Day13()
+}
+
+class Day13 {
+
+    init {
+        part1()
+//        part2()
+    }
+
+    var waitTime = 0
+
+    private fun part1() {
+        readFileAsLinesUsingUseLines("day13.txt").forEach {
+            if (waitTime == 0) {
+                waitTime = it.toInt()
+            } else {
+                it.split(",")
+                    .filter { it != "x" }
+                    .map { it.toInt() }
+                    .map { Pair(waitTime + it - waitTime % it, it) }
+                    .minBy { it.first }
+                    ?.let {
+                        println("${(it.first - waitTime) * it.second}")
+                    }
+            }
+        }
+    }
+
+    private fun part2() {
+        File("inputs/day13.txt").readLines()[1].split(",").withIndex().filter { it.value != "x" }
+            .joinToString(" && ") { (i, v) -> "(x+$i) mod $v = 0" }.let { URLEncoder.encode(it) }
+            .let { eq -> Desktop.getDesktop().browse(URL("https://www.wolframalpha.com/input/?i=$eq").toURI()) }
+
+
+    }
+}
